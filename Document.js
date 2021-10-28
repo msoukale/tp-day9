79d965b9aff5bc36ac34ae93295c08e89124e1e5
Moucharaf Soukale <soukalemoucharaf@gmail.com>
2021-10-28 14:20:14 +0200

    first commit

---
+++ Document.js
@@ -0,0 +1,13 @@
+//alert("HELLO")
+
+
+function myFunction() {
+
+ let a = prompt("donne moi un nombre:", "2");
+ let b = prompt("donne moi un autre:", "9");
+ let c = prompt("l'addition de ces deux nombres vaut:", "11");
+ let d = prompt("la multiplication de ces deux nombres vaut:", "18");
+}
+
+
+
---
+++ message.js
@@ -0,0 +1,10 @@
+//submitform() 
+      function submitform() {
+             var x = Math.floor((Math.random() *101));
+ 
+      alert("le nombre généré est "+ x); 
+                                           }   
+
+ 
+
+
